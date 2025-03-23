8558b4921a5b43ddc5837e087de42c92477580cc
artem1214 <61391756+artemanoh@users.noreply.github.com>
2025-03-23 10:43:04 +0200

    make Artem

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const students = [
         photo: "./img/mark.jpeg",
         age: "12",
         hobby: "Drone, bicycle"
+    },
+    {
+        studentName: "Artem",
+        photo: "./img/artem.jpg",
+        age: "16",
+        hobby: "Singing, dancing"
     }
 ]
