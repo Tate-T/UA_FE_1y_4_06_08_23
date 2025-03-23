4cfa229a60f90e40faa01454270a4f4821161a70
Timka09 <tymofij.i.naumenko@gmail.com>
2025-03-23 11:01:20 +0200

    🤑

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const students = [
-    {
-        studentName: "Mark",
-        photo: "./img/mark.jpeg",
-        age: "12",
-        hobby: "Drone, bicycle"
-    },
-    {
-        studentName: "Artem",
-        photo: "./img/artem.jpg",
-        age: "16",
-        hobby: "Singing, dancing"
-    },
-    {
-        studentName: "Vagus",
-        photo: "./img/vagus.jpg",
-        age: "13",
-        hobby: "3D model, cooking"
-    },
-    {
-        studentName: "Dima",
-        photo: "./img/vagus.jpg",
-        age: "13",
-        hobby: "biking, like turkeys"
-    },
-    {
-        studentName: "Olha",
-        photo: "./img/olha.jpg",
-        age: "15",
-        hobby: "fishing, guitar"
-    },
-    {
-        studentName: "Max",
-        photo: "./img/max.jpg",
-        age: "14",
-        hobby: "football, swimming"
-    }
-]
+  {
+    studentName: "Mark",
+    photo: "./img/mark.jpeg",
+    age: "12",
+    hobby: "Drone, bicycle",
+  },
+  {
+    studentName: "Artem",
+    photo: "./img/artem.jpg",
+    age: "16",
+    hobby: "Singing, dancing",
+  },
+  {
+    studentName: "Vagus",
+    photo: "./img/vagus.jpg",
+    age: "13",
+    hobby: "3D model, cooking",
+  },
+  {
+    studentName: "Dima",
+    photo: "./img/vagus.jpg",
+    age: "13",
+    hobby: "biking, like turkeys",
+  },
+  {
+    studentName: "Olha",
+    photo: "./img/olha.jpg",
+    age: "15",
+    hobby: "fishing, guitar",
+  },
+  {
+    studentName: "Max",
+    photo: "./img/max.jpg",
+    age: "14",
+    hobby: "football, swimming",
+  },
+  {
+    studentName: "TIMOHA",
+    photo: "./img/TIMOHA.jpg",
+    age: "15",
+    hobby: "venam",
+  },
+];
